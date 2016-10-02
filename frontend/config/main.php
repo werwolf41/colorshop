71c0fea2aa5c9810baecc1f0faf4722d15515e80
werwolf4 <werwolf41@gmail.com>
2016-10-02 10:38:00 +0300

    Перенесены настройки ресайза

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -5,54 +5,7 @@ $params = array_merge(
     require(__DIR__ . '/params.php'),
     require(__DIR__ . '/params-local.php')
 );
-Yii::$container->set('pavlinter\display\DisplayImage', [
-    //'returnSrc' => false,
-    //'mode' => DisplayImage::MODE_INSET,
-    'defaultImage' => 'default.jpg',
-    //'bgColor' => '000000',
-    //'bgAlpha' => 0,
-    'cacheDir' => '@webroot/admin/upload/cache',
-    'cacheWebDir' => '@web/admin/upload/cache',
-    //'innerCacheDir' => null, //example: 'cacheDirectory' takes precedence over [[cacheDir]]
-    //'generalDefaultDir' => true
-    'defaultCategory' => 'all',
-    'config' => [
-        'items' => [
-            'imagesWebDir' => '@web',
-            'imagesDir' => '@webroot',
-            'defaultWebDir' => '@web/admin/upload/',
-            'defaultDir' => '@webroot/admin/upload/',
-            'mode' => \pavlinter\display\DisplayImage::MODE_STATIC,
-        ],
-        'all' => [
-            'imagesWebDir' => '@web',
-            'imagesDir' => '@webroot',
-            'defaultWebDir' => '@web/admin/upload/',
-            'defaultDir' => '@webroot/admin/upload/',
-            'mode' => \pavlinter\display\DisplayImage::MODE_OUTBOUND,
-        ],
-        'users' => [
-            'imagesWebDir' => '@web',
-            'imagesDir' => '@webroot',
-            'defaultWebDir' => '@web/admin/upload/',
-            'defaultDir' => '@webroot/admin/upload/',
-            'mode' => 'ownMode',
-            'bgColor' => 'ff0000',
-            'resize' => function ($sender, $originalImage) {
-                /* @var $sender \pavlinter\display\DisplayImage */
-                /* @var $originalImage \Imagine\Imagick\Image */
 
-                $Box = new \Imagine\Image\Box($sender->width, $sender->height);
-                $newImage = $originalImage->thumbnail($Box);
-
-                $point = new \Imagine\Image\Point(0, 0);
-                $color = new \Imagine\Image\Color($sender->bgColor, $sender->bgAlpha);
-
-                return yii\imagine\Image::getImagine()->create($Box, $color)->paste($newImage, $point);
-            },
-        ],
-    ]
-]);
 return [
     'id' => 'app-frontend',
     'language'=>'ru-RU',
